2dfd48420b27dea3ffcbb7433e19526fa25f6487
Clayton Menezes dos Santos <claytonmenezes@hotmail.com>
2024-03-30 17:31:21 -0300

    primeiro commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="grid grid-cols-12 gap-10 pt-10">
+    <div class="col-span-12 flex justify-center">
+      <img src="../../public/imagem3.png" style="width: 10%; height: 100%;">
+      <div class="font-['Curlz_MT'] text-5xl sm:text-6xl md:text-8xl lg:text-9xl pt-20 text-blue-300">Chá da Alice</div>
+      <img src="../../public/imagem2.png" style="width: 10%; height: 100%;">
+    </div>
+    <div class="col-span-12 flex flex-wrap gap-10 pl-28 sm:justify-center">
+      <div v-for="(grupo, index) in grupos" :key="index">
+        <div class="inline-block text-xl font-semibold border-solid p-1 border-4 rounded-full border-blue-300 px-5 mb-4 ">
+          {{ grupo.nome }}
+        </div>
+        <div v-for="(item, index) in grupo.itens" :key="index" class="pb-1">
+          <input type="checkbox" v-model="item.selecionado" />
+          {{ item.nome }}
+        </div>
+      </div>
+    </div>
+    <div class="col-span-12 flex justify-center pb-10">
+      <Modal/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Modal from './Modal.vue'
+
+const grupos = ref([
+  {
+    nome: 'Fraldas',
+    itens: [
+      {nome: 'Fralda M', selecionado: false},
+      {nome: 'Fralda G', selecionado: false},
+    ]
+  },
+  {
+    nome: 'Quarto',
+    itens: [
+      {nome: 'Berço desmontável', selecionado: false},
+      {nome: 'Jogos de lençol para berço desmontável', selecionado: false},
+      {nome: 'Mantas', selecionado: false},
+      {nome: 'Cobertores de berço', selecionado: false},
+      {nome: 'Cobertores de enrolar', selecionado: false},
+      {nome: 'Colchonete para berço desmontável', selecionado: false},
+      {nome: 'Jogos de lençol para berço', selecionado: false},
+      {nome: 'Fronhas avulsas', selecionado: false},
+      {nome: 'Travesseiro antirrefluxo', selecionado: false},
+      {nome: 'Travesseiros antissufocante', selecionado: false},
+      {nome: 'Posicionador para dormir', selecionado: false},
+      {nome: 'Kits para berço', selecionado: false},
+      {nome: 'Mosquiteiro', selecionado: false},
+      {nome: 'Kits de fralda de boca', selecionado: false},
+      {nome: 'Blanket ( naninha )', selecionado: false},
+      {nome: 'Ninho', selecionado: false},
+      {nome: 'Móbile', selecionado: false},
+      {nome: 'Kits de cabide', selecionado: false},
+    ]
+  },
+  {
+    nome: 'Roupinhas de bebê',
+    itens: [
+      {nome: 'Fralda RN', selecionado: false},
+      {nome: 'Fralda P', selecionado: false},
+      {nome: 'Fralda M', selecionado: false},
+      {nome: 'Fralda G', selecionado: false},
+    ]
+  }
+])
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
+
+<template>
+    <button class="rounded-full bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300" style="width: 100px; height: 50px;" @click="open = true">
+        Finalizar
+    </button>
+    <transition name="fade">
+        <div v-if="open" class="fixed bg-blue-100 h-52 w-56 rounded-xl grid justify-center items-center border border-blue-300 shadow p-4 max-w-sm mx-auto" style="top: 30%; left: 50%; transform: translate(-50%, -50%);">
+            <div class="text-xl block font-medium text-slate-700">Digite o seu nome</div>
+            <input class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none" type="text">
+            <button class="mt-4 rounded-full bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300" style="height: 36px;" @click="open = false">
+                Salvar
+            </button>
+        </div>
+    </transition>
+</template>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 0.3s;
+}
+.fade-enter-from, .fade-leave-to {
+    opacity: 0;
+}
+</style>

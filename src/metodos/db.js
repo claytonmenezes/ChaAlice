05a9c0dba0c2ceb07f1f21e17268d86daed281c8
Clayton Menezes dos Santos <claytonmenezes@hotmail.com>
2024-04-04 23:41:29 -0300

    tentando um http

--- src/metodos/db.js
+++ src/metodos/db.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 
 const listar = async () => {
     const itens = await axios({
-        baseURL: 'http://localhost:6008',
+        baseURL: 'http://200.98.144.254:6008',
         url: 'itens'
     }).then(res => res.data)
     return itens
 }
 const atualizar = async (itensSelecionados) => {
     await axios({
-        baseURL: 'http://localhost:6008',
+        baseURL: 'http://200.98.144.254:6008',
         method: 'put',
         url: 'itens',
         data: {itensSelecionados}
